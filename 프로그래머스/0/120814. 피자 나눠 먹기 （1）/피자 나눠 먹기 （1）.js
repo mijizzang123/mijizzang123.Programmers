102fc0d8f0bc00a0874387a7dc962ee3638e8da6
MIJIZZANG123 <meezi_@naver.com>
2024-12-20 22:15:24 +0900

    [level 0] Title: 피자 나눠 먹기 (1), Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.js
@@ -0,0 +1,14 @@
+function solution(n) {
+    var answer = 0;
+    if(n<=7){
+        answer = 1;
+    }
+    if(n>7){
+        if(n%7 !=0)
+            answer = Math.floor(n/7)+1;
+        
+        else
+            answer = Math.floor(n/7);
+    }
+    return answer;
+}
